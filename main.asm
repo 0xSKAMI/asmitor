f18b97c3460d14f6f03e01885ed23e4d82c6d534
JSlayer93 <saba.manchkhashvili23@gmail.com>
2025-03-15 19:20:10 +0400

    removed some code did basic input system

--- main.asm
+++ main.asm
@@ -1,41 +1,41 @@
 section .data
 	filename db 'test.txt'
+	introduce db 'Put file name here: '
 
 section .bss
-	info resb 11 
-	fd_out resb 1
-	fd_in  resb 1
+	input resb 10 
+	input_length resb 1
 
 section .text
 	global _start
 
 _start:
-	;open file
-	mov eax, 5			;using sys_open
-	mov ebx, filename	;giving it filename
-	mov ecx, 0			;declaring mode permissions
-	int 0x80			;starting inteupt
-	
-	mov [fd_in], eax
+	;printing introduce text
+	mov eax, 4			;system_write
+	mov ebx, 1			;std_out
+	mov ecx, introduce	;introduce text
+	mov edx, 20			;bytes to output
+	int 0x80			;make system call
 
-	;reading from file
-	mov eax, 3			;using sys_read
-	mov ebx, [fd_in]	;file descriptor 
-	mov ecx, info		;pointer to buffer
-	mov edx, 26			;buffer size
-	int 0x80
+	;reading user input
+	mov eax, 3			;system_read
+	mov ebx, 0			;std_in
+	mov ecx, input		;info pointer
+	mov edx, 10			;read 10 bytes
+	int 0x80			;make system call
 
-	;printing result
-	mov eax, 4			
-	mov ebx, 1
-	mov ecx, info
-	mov edx, 11
-	int 0x80	
-   
-	; close the file
-	mov eax, 6
-	mov ebx, [fd_in]
-	int  0x80    
+	;unconditional jump to get_length label
+	mov edx, 110
+	cmp edx, 10
+	jl get_length	
+
+	get_length:
+		;printing user's input
+		mov eax, 4			;system_write
+		mov ebx, 1			;std_out
+		mov ecx, input_length		;user's input
+		mov edx, 10			;bytes to write
+		int 0x80			;make system call
 
 	mov eax, 1
 	int 0x80
