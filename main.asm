8bdeb4859f928f57ea6521b3732af7fc3c29d321
skamiXD <saba.manchkhashvili23@gmail.com>
2025-04-10 15:17:01 +0400

    changed everything for 64-bit systems

--- main.asm
+++ main.asm
@@ -14,59 +14,53 @@ section .text
 main:   
 	;printing introduce text  
 	mov rax, 1			;system_write  
-	mov rbx, 1			;std_out  
+	mov rdi, 1			;std_out  
 	mov rsi, introduce	;introduce text
 	mov rdx, 21			;bytes to output
 	syscall			;make system call  
-  
-	;allocating memory using malloc 
-	mov rdi, 40 
-	extern malloc 
-	call malloc 
-	ret	 
- 
+	
 	;reading user input 
-	mov eax, 3			;system_read 
-	mov ebx, 0			;std_in 
-	mov ecx, input		;input pointer
-	mov edx, 10			;read 10 bytes 
+	mov rax, 0			;system_read 
+	mov rdi, 0			;std_in 
+	mov rsi, input		;input pointer
+	mov rdx, 10			;read 10 bytes 
 	syscall			;make system call 
 	 
 	;adding null terminator in the end of file
-	mov byte [input + eax - 1], 0 
+	mov byte [input + rax - 1], 0 
  
 	;jump to exit if input is empty 
-	cmp eax, 1 
+	cmp rax, 1 
 	jle exit  
  
 	;opening file 
-	mov eax, 5						;using sys_open
-	mov ebx, input			  ;giving it filename
-	mov ecx, 2						;declaring mode permissions
+	mov rax, 2						;using sys_open
+	mov rdi, input			  ;giving it filename
+	mov rsi, 2						;declaring mode permissions
 	syscall							;starting inteupt
  
-	cmp eax, 0						 
+	cmp rax, 0						 
 	jl exit								;jump if file descriptor is negative
  
-	mov [fd_in], eax			;store the descriptor
+	mov [fd_in], rax			;store the descriptor
  
 	;reading from file 
-	mov eax, 3						;using sys_read
-	mov ebx, [fd_in]			;file descriptor
-	mov ecx, info					;info pointer	
-	mov edx, 26						;charachters to read
+	mov rax, 0						;using sys_read
+	mov rdi, [fd_in]			;file descriptor
+	mov rsi, info					;info pointer	
+	mov rdx, 26						;charachters to read
 	syscall							;run interrupt
  
 	;printing result 
-	mov eax, 4						;using sys_print
-	mov ebx, 1						;std_out file descriptor
-	mov ecx, info					;info pointer
-	mov edx, 11						;charachters to write
+	mov rax, 1						;using sys_write
+	mov rdi, 1						;std_out file descriptor
+	mov rsi, info					;info pointer
+	mov rdx, 26						;charachters to write
 	syscall							;run interrupt
     
 	; close the file 
-	mov eax, 3						;using sys_close
-	mov ebx, [fd_in]			;file descriptor
+	mov rax, 3						;using sys_close
+	mov rdi, [fd_in]			;file descriptor
 	syscall								;run interrupt
  
 	;label for exit code 
